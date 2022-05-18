e1aa3717d1867a70b4be1a112c5c778b12d69a95
RamonPadillax <41170274+ramonpjls@users.noreply.github.com>
2022-05-17 20:08:32 -0400

    feat: some changes:

--- src/axiosinstance.js
+++ src/axiosinstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instace = axios.create({
-  // baseURL: "https://sta-mangyver.herokuapp.com/api/v1",
+  baseURL: "https://sta-mangyver.herokuapp.com/api/v1",
   // baseURL: "http://localhost:3000/api/v1",
 
-  baseURL: "https://mangyver-back.azurewebsites.net/api/v1",
+  // baseURL: "https://mangyver-back.azurewebsites.net/api/v1",
 });
 
 instace.defaults.headers.common["auth"] = localStorage.token;
